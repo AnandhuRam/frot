cc394fb0462c4a18b6a255983f0d889dd95e92c7
= <anandhuram2021@gmail.com>
2021-10-10 21:31:44 +0530

    front

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-white h-20 sticky top-0 z-50 grid grid-cols-3 shadow-md py-5 px-5">
+        <div class="relative flex items-center my-auto ">
+            <h2 class="font-semibold text-xl text-gray-500">Employee Manager</h2>
+        </div>
+        <div class="flex items-center border-2  rounded-full ">
+            <input type="text" class="flex-grow pl-5 bg-transparent outline-none" placeholder="start your search">
+            <img class="relative h-10 bg-red-400 rounded-full p-1 cursor-pointer"
+             src="https://www.freeiconspng.com/thumbs/search-icon-png/search-icon-png-1.png" alt="search">
+            
+        </div>
+        <div class="flex items-center justify-end">
+           <img class="rounded-full h-10 cursor-pointer"
+           src="https://media.istockphoto.com/vectors/power-turn-off-iconvector-illustration-vector-id1018739074?k=20&m=1018739074&s=612x612&w=0&h=ZQYQXO8jYpWkoJzyk-bA1Ki0uF_LqmV2VGz8Skl5OZ4="  alt="">
+        </div>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name: 'Header',
+    
+}
+
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="fixed bg-blue-200"> 
+<div class="pt-5 pl-5 pb-5 pr-5 space-x-2  ">
+    <button class="pr-8 pl-8
+    bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+     ADD
+    </button>
+<button class="pr-8 pl-8
+    bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+     Delete
+    </button>
+
+</div>
+    <div class="max-w-xs bg-purple-200 h-screen rounded-lg " >
+        <h1 class="text-xl pl-5 pt-2 shadow-md cursor-pointer ">View </h1>
+        
+         <h1  class="text-xl pl-5 pt-2 cursor-pointer ">Time Info</h1>
+         <h1 class="text-xl pl-5 pt-2 shadow-md cursor-pointer ">Salary Info</h1>
+         <h1 class="text-xl pl-5 pt-2 cursor-pointer ">Working History</h1>
+         <h1 class="text-xl pl-5  pt-2 shadow-md cursor-pointer ">Emergrncy Contact</h1>
+
+    
+    </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name:'Sidebar',
+   
+    
+}
+</script>
